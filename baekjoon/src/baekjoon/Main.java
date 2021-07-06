245a11f179607d6fe27e2d7e9ea44fe2d9f23306
Hyun-Kim95 <75933619+Hyun-Kim95@users.noreply.github.com>
2021-07-06 16:25:57 +0900

    Add files via upload

--- baekjoon/src/baekjoon/Main.java
+++ baekjoon/src/baekjoon/Main.java
@@ -12,13 +12,13 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		int n = Integer.parseInt(br.readLine());
-		StringTokenizer st;
+		StringTokenizer st;	// 공백 단위로 데이터 나눠받기
 		for(int i=1;i<=n;i++) {
-			st = new StringTokenizer(br.readLine());
+			st = new StringTokenizer(br.readLine());	// 공백 단위로 데이터 나눠받기
 			bw.write((Integer.parseInt(st.nextToken()))+(Integer.parseInt(st.nextToken()))+"\n");
 		}
 		br.close();
-		bw.flush();
+		bw.flush();	// 남아있는 데이터를 모두 출력
 		bw.close();
 	}
 }
