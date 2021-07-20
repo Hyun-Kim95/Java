2f32e2f60675973e80e6740409b5f00810db8bb3
Hyun-Kim95 <75933619+Hyun-Kim95@users.noreply.github.com>
2021-07-20 16:32:54 +0900

    Add files via upload

--- baekjoon/src/baekjoon/Main.java
+++ baekjoon/src/baekjoon/Main.java
@@ -3,25 +3,20 @@ package baekjoon;
 import java.util.Scanner;
 
 public class Main {
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
 		int c = sc.nextInt();
-		for(int i=0;i<c;i++) {
-			int n = sc.nextInt();
-			int score[] = new int[n];
-			int sum = 0;
-			int cnt = 0;
-			for(int j=0;j<n;j++) {
-				score[j] = sc.nextInt();
-				sum += score[j];
+		int i = 0;
+		if(b<c){
+			while(a+b*i >= c*i) {
+				i++;
 			}
-			double avg = (double)sum / (double)n;
-			for(int k = 0;k<n;k++) {
-				if(score[k] >avg) {
-					cnt++;
-				}
-			}
-			System.out.println(String.format("%.3f",(double)cnt/(double)n*100)+"%");
+			System.out.println(i);
+		}else {
+			System.out.println(-1);
 		}
 	}
 }
