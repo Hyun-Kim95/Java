d3dbc97d7535c6e75292da6e35506d0cb8ac4da7
Hyun-Kim95 <75933619+Hyun-Kim95@users.noreply.github.com>
2021-08-18 17:42:47 +0900

    Add files via upload

--- baekjoon/src/baekjoon/Main.java
+++ baekjoon/src/baekjoon/Main.java
@@ -3,16 +3,22 @@ package baekjoon;
 import java.util.Scanner;
 
 public class Main {
+	public static int people(int a,int b) {
+		if(a==0)
+			return b;
+		else if(b==1)
+			return 1;
+		else
+			return people(a-1,b) + people(a,b-1);
+	}
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int a = sc.nextInt();
-		int i = 1;
-		int hang = 1;
-		int yul = 2;
-		while(a != i) {
-			hang
-			for(int j = 1;)
+		int t = sc.nextInt();
+		for (int i = 0; i < t; i++) {
+			int k = sc.nextInt();
+			int n = sc.nextInt();
+			System.out.println(people(k,n));				
 		}
-		
+		sc.close();
 	}
 }
